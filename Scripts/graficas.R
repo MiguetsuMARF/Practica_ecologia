5aab305ba4acb07c085821eb698a0f7f5e589f96
Ecoliuropatogena <ojacobo11@alumnos.uaq.mx>
2025-03-09 18:46:20 -0600

    guarde los datos de las tablas en csv
    guarde los datos que use ara is graficas a csv

--- Scripts/graficas.R
+++ Scripts/graficas.R
@@ -26,6 +26,13 @@ comunidad <- data.frame(
   bolsas = c("bolsa_1", "bolsa_2", "bolsa_3", "bolsa_4", "bolsa_5", "bolsa_6", "bolsa_7"),
   fichas = c(88,66,77,38,68,99,50)
 )
+View(comunidad)
+
+write.csv(comunidad, file ="Scripts/Tablas de graficas/comunidad.csv", row.names = FALSE)
+comunidad<-read.csv(file ="Scripts/Tablas de graficas/comunidad.csv")
+
+View(comunidad)
+
 
 comunidades <- ggplot(comunidad, aes(x = reorder(bolsas, fichas, decreasing = TRUE), y = fichas)) + 
   geom_point(pch = 25, colour = "brown4") + theme_linedraw()+
@@ -35,50 +42,70 @@ comunidades <- ggplot(comunidad, aes(x = reorder(bolsas, fichas, decreasing = TR
 
 
                # Extrayendo datos de las bolsas #
-Datos_graficas[which(Datos_graficas$bolsa == "Bolsa_1"),] -> bolsa_1
-Datos_graficas[which(Datos_graficas$bolsa == "Bolsa_2"),] -> bolsa_2
-Datos_graficas[which(Datos_graficas$bolsa == "Bolsa_3"),] -> bolsa_3
-Datos_graficas[which(Datos_graficas$bolsa == "Bolsa_4"),] -> bolsa_4
-Datos_graficas[which(Datos_graficas$bolsa == "Bolsa_5"),] -> bolsa_5
-Datos_graficas[which(Datos_graficas$bolsa == "Bolsa_6"),] -> bolsa_6
-Datos_graficas[which(Datos_graficas$bolsa == "Bolsa_7"),] -> bolsa_7
+Datos_graficas[which(Datos_graficas$bolsa == "Bolsa_1"),] -> b_1
+write.csv(b_1, file ="Scripts/Tablas de graficas/b_1.csv", row.names = FALSE)
+b_1<-read.csv(file ="Scripts/Tablas de graficas/b_1.csv")
 
+Datos_graficas[which(Datos_graficas$bolsa == "Bolsa_2"),] -> b_2
+write.csv(b_2, file ="Scripts/Tablas de graficas/b_2.csv", row.names = FALSE)
+b_2<-read.csv(file ="Scripts/Tablas de graficas/b_2.csv")
+
+Datos_graficas[which(Datos_graficas$bolsa == "Bolsa_3"),] -> b_3
+write.csv(b_3, file ="Scripts/Tablas de graficas/b_3.csv", row.names = FALSE)
+b_3<- read.csv(file ="Scripts/Tablas de graficas/b_3.csv")
+
+
+Datos_graficas[which(Datos_graficas$bolsa == "Bolsa_4"),] -> b_4
+write.csv(b_4, file ="Scripts/Tablas de graficas/b_4.csv", row.names = FALSE)
+b_4<- read.csv(file ="Scripts/Tablas de graficas/b_4.csv")
+
+Datos_graficas[which(Datos_graficas$bolsa == "Bolsa_5"),] -> b_5
+write.csv(b_5, file ="Scripts/Tablas de graficas/b_5.csv", row.names = FALSE)
+b_5<- read.csv(file ="Scripts/Tablas de graficas/b_5.csv")
+
+Datos_graficas[which(Datos_graficas$bolsa == "Bolsa_6"),] -> b_6
+write.csv(b_6, file ="Scripts/Tablas de graficas/b_6.csv", row.names = FALSE)
+b_6<- read.csv(file ="Scripts/Tablas de graficas/b_6.csv")
+
+Datos_graficas[which(Datos_graficas$bolsa == "Bolsa_7"),] -> b_7
+write.csv(b_7, file ="Scripts/Tablas de graficas/b_7.csv", row.names = FALSE)
+b_7<- read.csv(file ="Scripts/Tablas de graficas/b_7.csv")
 
 
 
 # Graficas de distribución de fichas por color y en orden decendente #
 
-b1_graph <- ggplot(bolsa_1, aes(x = reorder(color, n_fichas, decreasing = TRUE), y= n_fichas)) +
+b1_graph <- ggplot(b_1, aes(x = reorder(color, n_fichas, decreasing = TRUE), y= n_fichas)) +
   geom_point(pch = 25, colour = "darkorange") + theme_linedraw()+
   theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
   labs(x = NULL)
 
-b2_graph <- ggplot(bolsa_2, aes(x = reorder(color, n_fichas, decreasing = TRUE), y= n_fichas)) +
+b2_graph <- ggplot(b_2, aes(x = reorder(color, n_fichas, decreasing = TRUE), y= n_fichas)) +
   geom_point(pch = 25, colour = "darkgreen") + theme_linedraw()+
   theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
   labs(x = NULL)
 
-b3_graph <- ggplot(bolsa_3, aes(x = reorder(color, n_fichas, decreasing = TRUE), y= n_fichas)) +
+b3_graph <- ggplot(b_3, aes(x = reorder(color, n_fichas, decreasing = TRUE), y= n_fichas)) +
   geom_point(pch = 25, colour = "purple4") + theme_linedraw()+
   theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
   labs(x = NULL)
 
-b4_graph <- ggplot(bolsa_4, aes(x = reorder(color, n_fichas, decreasing = TRUE), y= n_fichas)) +
+b4_graph <- ggplot(b_4, aes(x = reorder(color, n_fichas, decreasing = TRUE), y= n_fichas)) +
   geom_point(pch = 25, colour = "darkturquoise") + theme_linedraw()+
   theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
   labs(x = NULL)
 
-b5_graph <- ggplot(bolsa_5, aes(x = reorder(color, n_fichas, decreasing = TRUE), y= n_fichas)) +
+b5_graph <- ggplot(b_5, aes(x = reorder(color, n_fichas, decreasing = TRUE), y= n_fichas)) +
   geom_point(pch = 25, colour = "darkblue") + theme_linedraw()+
   theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
   labs(x = NULL)
 
-b6_graph <- ggplot(bolsa_6, aes(x = reorder(color, n_fichas, decreasing = TRUE), y= n_fichas)) +
+b6_graph <- ggplot(b_6, aes(x = reorder(color, n_fichas, decreasing = TRUE), y= n_fichas)) +
   geom_point(pch = 25, colour = "deepskyblue") + theme_linedraw()+
   theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
   labs(x = NULL)
 
-b7_graph <- ggplot(bolsa_7, aes(x = reorder(color, n_fichas, decreasing = TRUE), y= n_fichas)) +
+b7_graph <- ggplot(b_7, aes(x = reorder(color, n_fichas, decreasing = TRUE), y= n_fichas)) +
   geom_point(pch = 25, colour = "darkred") + theme_linedraw()+
   theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
   labs(x = NULL)
